83d2d6b230c160870c8bc3d138eb0d53d89bdccb
sourabhthakuriitm <sourabhthakurbhl@gmail.com>
2019-12-02 14:39:37 +0530

    initial code

--- log_in.py
+++ log_in.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 import time
 
+# taking username,phone or email as input
+username = input("Enter username,email or password")
+password = input("Enter your Password")
+
 # a new chrome session
 driver = webdriver.Chrome()
 driver.implicitly_wait(15)
@@ -14,7 +18,7 @@ login_field = driver.find_element_by_class_name("js-username-field")
 login_field.clear()
 
 # enter username
-login_field.send_keys("ENTER USERNAME HERE")
+login_field.send_keys(username)
 time.sleep(1)
 
 # get the password text
@@ -22,6 +26,6 @@ password_field = driver.find_element_by_class_name("js-password-field")
 password_field.clear()
 
 # enter password
-password_field.send_keys("ENTER PASSWORD HERE")
+password_field.send_keys(password)
 time.sleep(1)
 password_field.submit()
