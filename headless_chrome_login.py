3dcab43d10881bb60aff00774a19371a2fe75197
sourabhthakuriitm <sourabhthakurbhl@gmail.com>
2019-12-25 11:28:50 +0530

    OOP_based_chrome_loging

--- headless_chrome_login.py
+++ headless_chrome_login.py
@@ -4,36 +4,27 @@ import os
 import time
 import getpass
 
-# taking username,phone or email as input
-username = input("Enter username,email or password\n")
-password = getpass.getpass("Enter your Password\n")
+class log_in():
+    def __init__(self,username, password):
+        self.username = username
+        self.password = password
 
-# headless chrome
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=1920x1080")
+    def chrome_login(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=1920x1080")
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+        driver.get("https://twitter.com/login")
+        login_field = driver.find_element_by_class_name("js-username-field")
+        login_field.clear()
+        login_field.send_keys(self.username)
+        time.sleep(1)
+        password_field = driver.find_element_by_class_name("js-password-field")
+        password_field.clear()
+        password_field.send_keys(self.password)
+        time.sleep(1)
+        password_field.submit()
 
-# a new chrome session
-driver = webdriver.Chrome(chrome_options=chrome_options)
-# driver.implicitly_wait(2)
-# driver.maximize_window()
-
-# navigatoe to the application home page
-driver.get("https://twitter.com/login")
-
-# get the username textbox
-login_field = driver.find_element_by_class_name("js-username-field")
-login_field.clear()
-
-# enter username
-login_field.send_keys(username)
-time.sleep(1)
-
-# get the password text box and clear it
-password_field = driver.find_element_by_class_name("js-password-field")
-password_field.clear()
-
-# enter password
-password_field.send_keys(password)
-time.sleep(1)
-password_field.submit()
+if __name__ == "__main__":
+    l = log_in(input("Enter username,email or password\n"),getpass.getpass("Enter your Password\n"))
+    l.chrome_login()
